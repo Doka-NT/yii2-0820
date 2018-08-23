376ad0336dc2cba831f3dea871e389e888f82c48
Soshnikov Artem <213036@skobka.com>
2018-08-23 23:44:11 +0500

    Урок 2 - Форма фидбека

---
+++ views/site/feedback.php
@@ -0,0 +1,16 @@
+<?php
+
+use app\models\forms\FeedbackForm;
+use yii\widgets\ActiveForm;
+use yii\helpers\Html;
+
+/* @var $model FeedbackForm */
+
+?>
+<?php $form = ActiveForm::begin(); ?>
+	<?=$form->field($model, 'name')->textInput();?>
+	<?=$form->field($model, 'email')->textInput(['type' => 'email']);?>
+	<?=$form->field($model, 'text')->textarea();?>
+
+	<?=Html::submitButton('Отправить');?>
+<?php ActiveForm::end();?>
